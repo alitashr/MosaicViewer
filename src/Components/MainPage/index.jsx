d472b0300d2e6416c27d27c39e147371d0577336
Alita Shrestha <aelistha@gmail.com>
2021-12-23 16:31:39 +0545

    new image loads only after canvasdata is updated

--- src/Components/MainPage/index.jsx
+++ src/Components/MainPage/index.jsx
@@ -12,7 +12,7 @@ const MainPage = () => {
   const [loading, setLoading] = useState(true);
   const [mosaicLoadComplete, setMosaicLoadComplete] = useState(false);
 
-  const [mosaicFilename, setMosaicFilename]= useState('');
+  const [mosaicFilename, setMosaicFilename] = useState("");
   useEffect(() => {
     setLoading(true);
     loadDefaultMosaic();
@@ -23,18 +23,22 @@ const MainPage = () => {
     var defaultImage = new Image(); // Creates image object
     defaultImage.src = "https://images.explorug.com/mosaic/monalisa.jpg"; // Assigns converted image to image object
     defaultImage.crossOrigin = "Anonymous";
-   
+
     defaultImage.onload = function () {
       setCanvasSizeFromImage(defaultImage.width, defaultImage.height);
       setInputImage(defaultImage);
     };
 
-    getDefaultMosaicData("monalisa", function (res) {
-      const monalisaMosaicData = JSON.parse(res);
-      setMosaicCanvasData(monalisaMosaicData);
-    }, function(){
-      console.log('error while getting default data')
-    });
+    getDefaultMosaicData(
+      "monalisa",
+      function (res) {
+        const monalisaMosaicData = JSON.parse(res);
+        setMosaicCanvasData(monalisaMosaicData);
+      },
+      function () {
+        console.log("error while getting default data");
+      }
+    );
   };
 
   const setCanvasSizeFromImage = (wid, hgt) => {
@@ -49,42 +53,47 @@ const MainPage = () => {
     if (!imageFile) return;
     setLoading(true);
     setMosaicLoadComplete(false);
-    console.log("handleImageChange -> setLoading")
+    console.log("handleImageChange -> setLoading");
     console.time();
     var reader = new FileReader();
     reader.readAsDataURL(imageFile);
     reader.onloadend = function (e) {
-       console.timeLog();
-      var myImage = new Image(); 
-      myImage.src = e.target.result; 
+      console.timeLog();
+      var myImage = new Image();
+      myImage.src = e.target.result;
       let filename = getFilename(imageFile.name);
       setMosaicFilename(filename);
       let fileType = imageFile.type;
       myImage.onload = function (ev) {
-      console.log("handleImageChange -> myImage", myImage.width, myImage.height )
+        console.log("handleImageChange -> myImage", myImage.width, myImage.height);
         console.timeLog();
         setCanvasSizeFromImage(myImage.width, myImage.height);
-      
-        var doubleTileFile = getResizedFile(myImage, fileType, true);
-        setInputImage(myImage);
-        myImage.onload = null
-        uploadDesign(doubleTileFile, filename, function (res) {
-          console.log("res", res);
-          console.timeLog();
 
-          if (res && res !== "" && res !== "maxsize" && res[0] !== "<") {
-            //sessionStorage.setItem("res", res);
-            const mosaicData = JSON.parse(res);
-           
-            setMosaicCanvasData(mosaicData);
-          } else {
-            console.log("response is not json");
+        var doubleTileFile = getResizedFile(myImage, fileType, true);
+        myImage.onload = null;
+        uploadDesign(
+          doubleTileFile,
+          filename,
+          function (res) {
+            console.log("res", res);
+            console.timeLog();
+
+            if (res && res !== "" && res !== "maxsize" && res[0] !== "<") {
+              //sessionStorage.setItem("res", res);
+              const mosaicData = JSON.parse(res);
+              setInputImage(myImage);
+
+              setMosaicCanvasData(mosaicData);
+            } else {
+              console.log("response is not json");
+              setLoading(false);
+            }
+          },
+          function () {
+            console.log("Error while uploading image");
             setLoading(false);
           }
-        }, function(){
-          console.log("Error while uploading image");
-          setLoading(false);
-        });
+        );
       };
     };
   };
@@ -92,27 +101,30 @@ const MainPage = () => {
     console.log("handleOnLoadComplete -> handleOnLoadComplete");
     setLoading(false);
   };
-  const handleOnLoadComplete=()=>{
-  console.log("handleOnLoadComplete -> handleOnLoadComplete");
-  setLoading(false);
-  setMosaicLoadComplete(true)
-  }
-  const downloadMosaic=()=>{
+  const handleOnLoadComplete = () => {
+    console.log("handleOnLoadComplete -> handleOnLoadComplete");
+    setLoading(false);
+    setMosaicLoadComplete(true);
+  };
+  const downloadMosaic = () => {
     console.log("downloadMosaic -> downloadMosaic");
-    var downloadCanvas = document.createElement('canvas');
+    var downloadCanvas = document.createElement("canvas");
     var downloadCanvasContext = downloadCanvas.getContext("2d");
-  
+
     var mosaicCanvas = document.getElementById("inputMosaicImage");
     downloadCanvas.width = mosaicCanvas.width;
     downloadCanvas.height = mosaicCanvas.height;
     downloadCanvasContext.drawImage(mosaicCanvas, 0, 0, downloadCanvas.width, downloadCanvas.height);
-   
+
     var imageCanvas = document.getElementById("transformComponentCanvas");
     downloadCanvasContext.globalAlpha = 0.65;
     downloadCanvasContext.drawImage(imageCanvas, 0, 0, imageCanvas.width, imageCanvas.height);
-    const filename =  mosaicFilename && mosaicFilename!='' ? mosaicFilename.substring(0, mosaicFilename.lastIndexOf('.')):'MonalisaMosaic';
+    const filename =
+      mosaicFilename && mosaicFilename != ""
+        ? mosaicFilename.substring(0, mosaicFilename.lastIndexOf("."))
+        : "MonalisaMosaic";
     downloadImageData(downloadCanvas, `${filename}-mosaic.jpg`, "jpg");
-  }
+  };
 
   return (
     <>
@@ -127,9 +139,9 @@ const MainPage = () => {
       )}
 
       <ImageDropContainer onImageChange={handleImageChange} />
-        <Button disabled={loading} type="primary" className="download-button" onClick={downloadMosaic}>
-            Download Image
-          </Button>
+      <Button disabled={loading} type="primary" className="download-button" onClick={downloadMosaic}>
+        Download Image
+      </Button>
 
       {loading && <Spinner />}
       <div id="canvasArea">
